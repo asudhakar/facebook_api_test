6791723d45f22dd6d7b6eafc386c1d740d72d00c
sudhakar <asudhakar@live.in>
2016-06-27 22:44:00 +0530

    test

--- index.php
+++ index.php
@@ -8,22 +8,14 @@ $fb = new Facebook\Facebook([
   'default_graph_version' => 'v2.5'
 ]);
 
-$linkData = [
-  'link' => 'http://www.example.com',
-  'message' => 'User provided message',
-  ];
 
-try {
-  // Returns a `Facebook\FacebookResponse` object
-  $response = $fb->post('/me/feed', $linkData, '{3f179db54a9bf1b1713d69fa539dc2d3}');
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-
-$graphNode = $response->getGraphNode();
-
-echo 'Posted with id: ' . $graphNode['id'];
+$request = new FacebookRequest(
+  $session,
+  'POST',
+  '/250674131760799/members',
+  array (
+    'member' => '1',
+  )
+);
+$response = $request->execute();
+$graphObject = $response->getGraphObject();
