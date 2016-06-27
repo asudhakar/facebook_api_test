a35f9804ea2b4e811a45e1da24977164e33ed743
sudhakar <asudhakar@live.in>
2016-06-27 22:46:58 +0530

    test

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 
 require_once __DIR__ . '/Facebook/autoload.php';
+require_once __DIR__ . '/Facebook/FacebookRequest.php';
 $fb = new Facebook\Facebook([
   'app_id' => '{506734579516110}',
   'app_secret' => '{941bd3c973033e6245842eb1ed1b169e}',
@@ -14,7 +15,7 @@ $request = new FacebookRequest(
   'POST',
   '/250674131760799/members',
   array (
-    'member' => '1'
+    'member' => '1234567',
   )
 );
 $response = $request->execute();
