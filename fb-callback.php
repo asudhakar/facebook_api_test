d1bbdf2fc35d8b17a0829a9fe2e820783f797df2
sudhakar <asudhakar@live.in>
2016-06-27 22:28:21 +0530

    app id app secret will  be added

--- fb-callback.php
+++ fb-callback.php
@@ -3,10 +3,10 @@
 
 require_once __DIR__ . '/Facebook/autoload.php';
 $fb = new Facebook\Facebook([
-  'app_id' => '{app-id}', // Replace {app-id} with your app id
-  'app_secret' => '{app-secret}',
-  'default_graph_version' => 'v2.2',
-  ]);
+  'app_id' => '{506734579516110}',
+  'app_secret' => '{941bd3c973033e6245842eb1ed1b169e}',
+  'default_graph_version' => 'v2.5'
+]);
 
 $helper = $fb->getRedirectLoginHelper();
 
