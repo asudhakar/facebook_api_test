e6a7115cad89ddf2cfd6743dfd71843acab5520a
sudhakar <asudhakar@live.in>
2016-06-27 22:18:12 +0530

    test

--- index.php
+++ index.php
@@ -7,3 +7,23 @@ $fb = new Facebook\Facebook([
   'app_secret' => '{941bd3c973033e6245842eb1ed1b169e}',
   'default_graph_version' => 'v2.5'
 ]);
+
+$linkData = [
+  'link' => 'http://www.example.com',
+  'message' => 'User provided message',
+  ];
+
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->post('/me/feed', $linkData, '{access-token}');
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$graphNode = $response->getGraphNode();
+
+echo 'Posted with id: ' . $graphNode['id'];
